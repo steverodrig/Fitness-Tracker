61436c87dfd762d674b47c6cae3b9fe2ab338be7
Steven Rodriguez <srrodrig.rodriguez@gmail.com>
2020-11-18 15:02:51 -0600

    Added notes and a readme file

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Fitness-Tracker
+# Fitness Tracker
+
+  <img src =https://img.shields.io/badge/license-MIT-brightgreen alt = "license badge">
+
+  ## Description
+
+  This project utilizes MongoDb and Mongoose in order to store and retrieve data on the users workout exercises.
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [License](#license)
+
+  ## Installation
+
+  To install, first clone this repo.  Run NPM install, ensure you have mongoDb running on your computer, then type "npm run seed". 
+  
+  ## Usage
+ 
+  Use the start page to either continue a previous workout or add a new exercise.  You can view your history on the dashboard page to track your progress.
+
+  ## Contributing
+
+  Email me with your ideas and i will add you as a contributor, or simply clone and build on your own.
+
+  ## Tests
+
+  n/a
+
+  ## Questions
+
+  Contact me with any questions or concerns at the email link below.
+  
+  [Github profile](https://github.com/steverodrig) 
+
+  <a href = "mailto: sr_rodrig@yahoo.com">Send Email</a>
+
+  ## License
+
+  Licensed under MIT.
+
+  ## Links
+
+  Deployed version: 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,57 +1,63 @@
 const db = require("../models");
 const router = require("express").Router();
 
+
+// Pull all data from workouts collection
 router.get("/api/workouts", (req, res) => {
     db.Workout.find({})
-    .then(dbItem => {
-        res.json(dbItem);
-    })
-    .catch(err => {
-        res.json(err);
-    })
+        .then(dbItem => {
+            res.json(dbItem);
+        })
+        .catch(err => {
+            res.json(err);
+        })
 }
-
 )
+
+// Pull last 7 days of exercises
 router.get("/api/workouts/range", (req, res) => {
     db.Workout.find({}).limit(7)
-    .then(dbItem => {
-        res.json(dbItem);
-    })
-    .catch(err => {
-        res.json(err);
-    })
+        .then(dbItem => {
+            res.json(dbItem);
+        })
+        .catch(err => {
+            res.json(err);
+        })
 })
 
+// Add new workouts
 router.post("/api/workouts", ({ body }, res) => {
     db.Workout.create(body)
-    .then(dbItem => {
-        res.json(dbItem);
-    })
-    .catch(err => {
-        res.json(err);
-    })
+        .then(dbItem => {
+            res.json(dbItem);
+        })
+        .catch(err => {
+            res.json(err);
+        })
 
 })
 
+// Update existing workout plan
 router.put("/api/workouts/:id", (req, res) => {
-    db.Workout.findByIdAndUpdate(req.params.id, {$push: { exercises: req.body }}, { new: true, runValidators: true })
-    .then(dbItem => {
-        res.json(dbItem);
-    })
-    .catch(err => {
-        res.json(err);
-    })
+    db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true, runValidators: true })
+        .then(dbItem => {
+            res.json(dbItem);
+        })
+        .catch(err => {
+            res.json(err);
+        })
 
 })
 
+// Delete collection entry
 router.delete("/api/workouts", ({ body }, res) => {
     db.Workout.findByIdAndDelete(body.id)
-    .then(() => {
-        res.json(true);
-    })
-    .catch(err => {
-        res.json(err);
-    })
+        .then(() => {
+            res.json(true);
+        })
+        .catch(err => {
+            res.json(err);
+        })
 })
 
 module.exports = router;
